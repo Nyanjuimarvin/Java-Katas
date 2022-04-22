fa02ce033c5b2b91bcc1795eccf4398bbacb6b44
marvin <marnyanjui@gmail.com>
2022-04-22 20:42:06 +0300

    Add Kata description

---
+++ src/main/java/CollectionOps/CollectionOps.java
@@ -0,0 +1,15 @@
+package CollectionOps;
+
+/*
+*
+* Context: A common operation is needing to
+*  swap the order of items in a collection of data
+*  Task: Create a program that will:
+* reverse the order of a list if the list consists of integers alone,
+* Will convert the list items to uppercase if the list contains words only.
+* Will return the same list if the list members don’t satisfy either of these two criteria
+
+* */
+
+public class CollectionOps {
+}
